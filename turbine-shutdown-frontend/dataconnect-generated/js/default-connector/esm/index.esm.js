997d90e2bbed576f88e9059fbadbc9a5af382e5f
TIWARINEERAJ <your_email@example.com>
2025-04-18 13:26:46 +0530

    Add untracked files from turbine-shutdown-frontend

---
+++ turbine-shutdown-frontend/dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'turbine-shutdown-service',
+  location: 'asia-south1'
+};
+
---
+++ turbine-shutdown-frontend/dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'turbine-shutdown-service',
+  location: 'asia-south1'
+};
+exports.connectorConfig = connectorConfig;
